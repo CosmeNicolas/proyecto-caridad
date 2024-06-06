b8ce722195eaa1c5aa55da078ef5da2a72402646
CosmeNicolas <nicomas7590@gmail.com>
2024-06-06 15:39:04 -0300

    agregue los link de las redes sociales

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,23 +2,55 @@ import React from 'react'
 import { BsTwitterX } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFillThreadsFill } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#445D48] py-4 font-oswald  flex  justify-around">
-      <article className='text-center'>
-      <p className="text-lg text-slate-200">
-          &copy; Todos los derechos reservados
-        </p>
-        <p className=" text-slate-200 text-lg ">Hecho con 💚 </p>
+    <footer className="bg-[#445D48] py-4 font-oswald  flex flex-col md:flex-row md:justify-around ">
+      <article className="text-center">
+        <div className="bg-azul-oscuro rounded-xl p-4">
+          <p className="text-lg text-slate-200">
+            &copy; Todos los derechos reservados
+          </p>
+          <p className=" text-slate-200 text-lg ">Hecho con 💚 </p>
+        </div>
       </article>
       <article>
-        <p className="text-lg text-slate-200">Sigueme</p>
-        <ul className='flex  justify-around text-slate-200 mt-1'>
-          <li><BsTwitterX /></li>
-          <li><BsFacebook /></li>
-          <li><BsInstagram /></li>
-        </ul>
+        <div className="bg-white rounded-md p-4 flex items-center justify-center flex-col mt-2">
+          <p className="text-lg text-azul-oscuro font-oswald font-bold mx-1">
+            Sigueme
+          </p>
+          <ul className="flex  justify-between   text-slate-200 mt-1">
+            <Link to="https://x.com/Nicolas50418799">
+              <li>
+                <BsTwitterX className="text-[#000] mx-1" />
+              </li>
+            </Link>
+            <Link to="https://www.facebook.com/Nico.Mas777?mibextid=ZbWKwL">
+              <li>
+                <BsFacebook className="text-[#1877F2] mx-1" />
+              </li>
+            </Link>
+            <Link to='https://www.instagram.com/cosme_nicolas/'>
+              <li>
+                <BsInstagram className="text-[#E1306C] mx-1" />
+              </li>
+            </Link>
+            <Link to='https://www.linkedin.com/in/cosmenicolas/'>
+              <li>
+                <BsLinkedin className="text-[#0A66C2] mx-1" />
+              </li>
+            </Link>
+            <Link to='https://www.threads.net/@cosme_nicolas'>
+              <li>
+                <BsFillThreadsFill className="text-[#000] mx-1" />
+              </li>
+            </Link>
+            
+          </ul>
+        </div>
       </article>
     </footer>
   );
