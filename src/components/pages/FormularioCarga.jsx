989c78091af67ff225584df0c9d996e40ba72569
CosmeNicolas <nicomas7590@gmail.com>
2024-06-16 22:00:44 -0300

    cambie en el boton cargando , la frase cargando donacion...

--- src/components/pages/FormularioCarga.jsx
+++ src/components/pages/FormularioCarga.jsx
@@ -248,13 +248,12 @@ const FormularioCarga = () => {
           {/* BTN ENVIO */}
           
           <Button
-         /*  isLoading */
-         isLoading={isLoading}
+            isLoading={isLoading}
             type="submit"
             className="bg-[#445D48] text-white px-4 py-2 rounded-md" 
             size="lg"
           >
-           {isLoading ? 'Cargando...' : 'Enviar'}
+           {isLoading ? 'Cargando Donación...' : 'Enviar'}
           </Button>
         </form>
       </div>
